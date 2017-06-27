9322b61ceba196c855f0b5617ecec28371a8d9e1
Alla P <evetrueivory@gmail.com>
2017-06-27 21:39:28 +0300

    Новый проект «список дел» на Vue.js.

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template xmlns:v-on="http://www.w3.org/1999/xhtml" xmlns:v-bind="http://www.w3.org/1999/xhtml">
+  <div id="app">
+	<h1>TO-DO</h1>
+
+	<my-main></my-main>
+  </div>
+</template>
+
+<script>
+    import Main from './components/Main.vue';
+
+    export default {
+        name: 'app',
+        data () {
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        },
+        components: {
+            'my-main': Main
+        }
+    }
+</script>
+
+<style>
+
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 20px;
+
+		display: flex;
+		flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+
+	}
+
+	h1, h2 {
+		font-weight: normal;
+	}
+
+	ul {
+		list-style-type: none;
+		padding: 0;
+	}
+
+	li {
+		display: inline-block;
+		margin: 0 10px;
+	}
+
+	a {
+		color: #42b983;
+	}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,54 @@
+<template xmlns:v-on="http://www.w3.org/1999/xhtml" xmlns:v-bind="http://www.w3.org/1999/xhtml">
+    <div id="main">
+        <years></years>
+        <months v-on:changeIt="toggleBool(showMonth)" v-bind:ok="bool"></months>
+        <schedule v-bind:ok="bool"></schedule>
+    </div>
+</template>
+
+<script>
+
+    import months from './Months.vue';
+    import schedule from './Schedule.vue';
+    import Years from './Years.vue';
+
+    export default {
+        name: '',
+        data () {
+            return {
+                bool: false
+            }
+        },
+        components: {
+            months: months,
+            schedule: schedule,
+            years: Years,
+        },
+        methods: {
+            toggleBool: function (m) {
+                this.bool = true;
+                console.log(m);
+            },
+
+            show: function () {
+                this.$emit('show')
+            }
+        },
+        computed: {
+            showMonth: function () {
+            return months.data().msg;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+    #main {
+        display: flex;
+        align-items: flex-start;
+        justify-content: center;
+        width: 100%;
+        height: 100%;
+    }
+</style>
